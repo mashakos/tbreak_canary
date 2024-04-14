fbfcb40f86f01b17d0599dd7e39d5d35db66b23e
Mohamed Al Arabi <mo.arabi@gmail.com>
2024-04-14 03:52:24 +0200

    added tina generated folder

---
+++ tina/__generated__/config.prebuild.jsx
@@ -0,0 +1,79 @@
+// tina/config.js
+import { defineConfig } from "tinacms";
+var branch = process.env.GITHUB_BRANCH || process.env.VERCEL_GIT_COMMIT_REF || process.env.HEAD || "main";
+var config_default = defineConfig({
+  branch,
+  // Get this from tina.io
+  clientId: process.env.NEXT_PUBLIC_TINA_CLIENT_ID,
+  // Get this from tina.io
+  token: process.env.TINA_TOKEN,
+  build: {
+    outputFolder: "admin",
+    publicFolder: "public"
+  },
+  media: {
+    tina: {
+      mediaRoot: "static",
+      publicFolder: "public"
+    }
+  },
+  // See docs on content modeling for more info on how to setup new content models: https://tina.io/docs/schema/
+  schema: {
+    collections: [
+      {
+        name: "post",
+        label: "Posts",
+        path: "app/routes",
+        format: "mdx",
+        ui: {
+          router: ({ document }) => {
+            return `/articles/${document._sys.basename.replace("articles.", "").replace(".mdx", "")}`;
+          },
+          filename: {
+            // if disabled, the editor can not edit the filename
+            readonly: true,
+            // Example of using a custom slugify function
+            slugify: (values) => {
+              return `articles.${values?.title?.toLowerCase().replace(/ /g, "-")}`;
+            }
+          }
+        },
+        fields: [
+          {
+            type: "string",
+            name: "title",
+            label: "Title",
+            isTitle: true,
+            required: true
+          },
+          {
+            type: "string",
+            name: "abstract",
+            label: "Abstract",
+            required: true
+          },
+          {
+            type: "image",
+            name: "banner",
+            label: "Banner"
+          },
+          {
+            type: "datetime",
+            name: "date",
+            label: "Date",
+            required: true
+          },
+          {
+            type: "rich-text",
+            name: "body",
+            label: "Body",
+            isBody: true
+          }
+        ]
+      }
+    ]
+  }
+});
+export {
+  config_default as default
+};

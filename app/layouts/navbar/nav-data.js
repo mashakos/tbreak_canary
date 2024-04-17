76f5bb569d944bc55d4674851194b1e5193b7e2e
Mohamed Al Arabi <mo.arabi@gmail.com>
2024-04-17 21:33:21 +0200

    updated site structure

--- app/layouts/navbar/nav-data.js
+++ app/layouts/navbar/nav-data.js
@@ -1,18 +1,18 @@
 import config from '~/config.json';
 
 export const navLinks = [
-  // {
-  //   label: 'Projects',
-  //   pathname: '/#project-1',
-  // },
+  {
+    label: 'Projects',
+    pathname: '/projects',
+  },
   // {
   //   label: 'Details',
   //   pathname: '/#details',
   // },
-  // {
-  //   label: 'Articles',
-  //   pathname: '/articles',
-  // },
+  {
+    label: 'Articles',
+    pathname: '/articles',
+  },
   {
     label: 'Contact',
     pathname: '/contact',
@@ -20,19 +20,19 @@ export const navLinks = [
 ];
 
 export const socialLinks = [
-  // {
-  //   label: 'Twitter',
-  //   url: `https://twitter.com/${config.twitter}`,
-  //   icon: 'twitter',
-  // },
+  {
+    label: 'Twitter',
+    url: `https://twitter.com/${config.twitter}`,
+    icon: 'twitter',
+  },
   // {
   //   label: 'Figma',
   //   url: `https://www.figma.com/${config.figma}`,
   //   icon: 'figma',
   // },
-  // {
-  //   label: 'Github',
-  //   url: `https://github.com/${config.github}`,
-  //   icon: 'github',
-  // },
+  {
+    label: 'Github',
+    url: `https://github.com/${config.github}`,
+    icon: 'github',
+  },
 ];

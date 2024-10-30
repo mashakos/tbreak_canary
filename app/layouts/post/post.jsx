488c57595903dd39d63127580c4496421c4a7002
Mohamed Al Arabi <mo.arabi@gmail.com>
2024-10-30 09:01:39 +0300

    cloudflare image transformations test

--- app/layouts/post/post.jsx
+++ app/layouts/post/post.jsx
@@ -42,7 +42,7 @@ export const Post = ({ children, timecode }) => {
     scrollToHash(event.currentTarget.href);
   };
 
-  const placeholder = data.post.banner_placeholder;
+  const placeholder = `/cdn-cgi/image/width=25,quality=75,blur=50${data.post.banner}`; //data.post.banner_placeholder;
 
   const tinaComponents = {
     // The "YoutubeEmbed" component renders YouTube urls.

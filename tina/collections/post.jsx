c8f18f471c02593e522ac8eafb74b9795ec9e8be
Mohamed Al Arabi <mo.arabi@gmail.com>
2024-11-05 13:10:21 +0200

    test add beforesubmit

--- tina/collections/post.jsx
+++ tina/collections/post.jsx
@@ -13,7 +13,134 @@ export default {
   format: 'mdx',
   ui: {
     // Example of beforeSubmit
+    beforeSubmit: async ({
+                           form,
+                           values,
+                         }) => {
+      // const replacer = (key, value) => {
+      //   if (key === "type" || key === "id") return undefined;
+      //   return value;
+      // };
+      console.log(`before submit triggered. date is ${values.date}`);
+      var bodydata = "";
+      var postSlug = form.id.replace('app/routes/articles.', '/articles/').replace(/\.mdx$/, '');
+      var postId = 0;
+      Object.entries(values.body.children).forEach(([k, v]) => {
+        // do something with key and val
+        //for(var i = 0; i < values.body[k].length; i++)
 
+          Object.entries(v).forEach(([k, v]) => {
+            if(k === "children")
+              if(v[0].text !== undefined)
+              {
+                bodydata = bodydata + v[0].text + '\n';
+              }
+          });
+        //console.log(JSON.stringify(k, null, 2) + ' - ' + JSON.stringify(v[0]["children"].text, null, 2));
+      });
+      console.log(bodydata);
+
+      // Object.keys(values.body).forEach(function([k, v]){
+      //   for(var i = 0; i < values.body[k].length; i++)
+      //   console.log(k + ' - ' + v);
+      // });
+      //console.log(JSON.stringify(values.body[k]));
+      //typesense test
+
+/*
+* local vars test
+*           'nodes': [{
+            'host': 'search.delosian.pro', // For Typesense Cloud use xxx.a1.typesense.net
+            'port': '443',      // For Typesense Cloud use 443
+            'protocol': 'https'  // For Typesense Cloud use https
+          }],
+          'apiKey': 'xyz',
+          'connectionTimeoutSeconds': 2,
+
+* */
+/*
+* remoet vars test
+*           'nodes': [{
+            'host': process.env.TYPESENSE_HOST, // For Typesense Cloud use xxx.a1.typesense.net
+            'port': parseInt(process.env.TYPESENSE_PORT),      // For Typesense Cloud use 443
+            'protocol': process.env.PUBLIC_TYPESENSE_PROTOCOL  // For Typesense Cloud use https
+          }],
+          'apiKey': process.env.TYPESENSE_API_KEY,
+          'connectionTimeoutSeconds': 2,
+* */
+      try{
+        let typesenseClient = new Typesense.Client({
+          'nodes': [{
+            'host': 'search.delosian.pro', // For Typesense Cloud use xxx.a1.typesense.net
+            'port': 443,      // For Typesense Cloud use 443
+            'protocol': 'https'  // For Typesense Cloud use https
+          }],
+          'apiKey': 'xyz',
+          'connectionTimeoutSeconds': 2,
+          // logLevel: "debug",
+        });
+
+        {
+          let searchParameters = {
+            'q'         : '*',
+            "filter_by": `slug:=${postSlug}`,
+          };
+          await typesenseClient.collections('post').documents().search(searchParameters).then(function (data) {
+            // console.log("search data - " + JSON.stringify(data, null, 2));
+            if(data.found !== 0)
+            {
+              postId = data.hits[0].document.id;
+              console.log(postId);
+            }
+          });
+
+          if(postId !== 0)
+          {
+            let postDocument = {
+              'id': postId,
+              'title': values.title,
+              'abstract': values.abstract,
+              'banner': values.banner,
+              'date': new Date(values.date).getTime(),
+              'body': bodydata,
+              'slug': postSlug,
+            };
+            await typesenseClient.collections('post').documents().upsert(
+              postDocument,
+              {"filter_by": `slug:=${postSlug}`}
+            ).then(function (data) {
+              console.log(data);
+            });
+          }
+          else
+          {
+            let postDocument = {
+              'title': values.title,
+              'abstract': values.abstract,
+              'banner': values.banner,
+              'date': new Date(values.date).getTime(),
+              'body': bodydata,
+              'slug': postSlug,
+            };
+            await typesenseClient.collections('post').documents().create(
+              postDocument
+            ).then(function (data) {
+              console.log(data);
+            });
+          }
+        }
+
+      }
+      catch (err) {
+        console.error(err);
+      }
+
+
+      return {
+        ...values,
+        lastUpdated: new Date().toISOString()
+      };
+    },
     router: ({ document }) => {
       // must remove file extension and prepeneded article.
       return `/articles/${document._sys.basename.replace('articles.', '').replace('.mdx', '')}`;

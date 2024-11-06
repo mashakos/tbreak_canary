113d9941f65263bf02fbc3ed48242ceaac9000ab
Mohamed Al Arabi <mo.arabi@gmail.com>
2024-11-06 21:06:08 +0200

    cloudflare env vars test #9 revert 2

--- tina/__generated__/config.prebuild.jsx
+++ tina/__generated__/config.prebuild.jsx
@@ -1,13 +1,98 @@
 // tina/config.js
 import { defineConfig } from "tinacms";
 
-// tina/collections/post.js
+// tina/collections/post.jsx
+import Typesense from "typesense";
 var post_default = {
   name: "post",
   label: "Posts",
   path: "app/routes",
   format: "mdx",
   ui: {
+    // Example of beforeSubmit
+    beforeSubmit: async ({
+      form,
+      values
+    }) => {
+      console.log(`before submit triggered. date is ${values.date}`);
+      var bodydata = "";
+      var postSlug = form.id.replace("app/routes/articles.", "/articles/").replace(/\.mdx$/, "");
+      var postId = 0;
+      Object.entries(values.body.children).forEach(([k, v]) => {
+        Object.entries(v).forEach(([k2, v2]) => {
+          if (k2 === "children") {
+            if (v2[0].text !== void 0) {
+              bodydata = bodydata + v2[0].text + "\n";
+            }
+          }
+        });
+      });
+      console.log(bodydata);
+      try {
+        let typesenseClient = new Typesense.Client({
+          "nodes": [{
+            "host": "search.delosian.pro",
+            // For Typesense Cloud use xxx.a1.typesense.net
+            "port": 443,
+            // For Typesense Cloud use 443
+            "protocol": "https"
+            // For Typesense Cloud use https
+          }],
+          "apiKey": "xyz",
+          "connectionTimeoutSeconds": 2
+          // logLevel: "debug",
+        });
+        {
+          let searchParameters = {
+            "q": "*",
+            "filter_by": `slug:=${postSlug}`
+          };
+          await typesenseClient.collections("post").documents().search(searchParameters).then(function(data) {
+            if (data.found !== 0) {
+              postId = data.hits[0].document.id;
+              console.log(postId);
+            }
+          });
+          if (postId !== 0) {
+            let postDocument = {
+              "id": postId,
+              "title": values.title,
+              "abstract": values.abstract,
+              "banner": values.banner,
+              "date": new Date(values.date).getTime(),
+              "body": bodydata,
+              "slug": postSlug
+            };
+            await typesenseClient.collections("post").documents().upsert(
+              postDocument,
+              { "filter_by": `slug:=${postSlug}` }
+            ).then(function(data) {
+              console.log(data);
+            });
+          } else {
+            let postDocument = {
+              "title": values.title,
+              "abstract": values.abstract,
+              "banner": values.banner,
+              "date": new Date(values.date).getTime(),
+              "body": bodydata,
+              "slug": postSlug
+            };
+            await typesenseClient.collections("post").documents().create(
+              postDocument
+            ).then(function(data) {
+              console.log(data);
+            });
+          }
+        }
+      } catch (err) {
+        console.error(err);
+      }
+      return {
+        ...values,
+        lastUpdated: (/* @__PURE__ */ new Date()).toISOString()
+      };
+    },
     router: ({ document }) => {
       return `/articles/${document._sys.basename.replace("articles.", "").replace(".mdx", "")}`;
     },
@@ -16,7 +101,7 @@ var post_default = {
       readonly: true,
       // Example of using a custom slugify function
       slugify: (values) => {
-        return `articles.${values?.title?.toLowerCase().replace(/ /g, "-")}`;
+        return `articles.${values?.title?.toLowerCase().replace(/[|&;$%@"<>()+,:]/g, "").replace(/ /g, "-")}`;
       }
     }
   },

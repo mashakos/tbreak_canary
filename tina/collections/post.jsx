efb652047e5e2a04f64023f4953ad07a5773b0dc
Mohamed Al Arabi <mo.arabi@gmail.com>
2025-03-26 02:09:17 +0200

    updated url slug regex

--- tina/collections/post.jsx
+++ tina/collections/post.jsx
@@ -115,8 +115,7 @@ export default {
         // Values is an object containing all the values of the form. In this case it is {title?: string, topic?: string}
         return `articles.${values?.title
           ?.toLowerCase()
-          .replace(/([^a-z0-9 -])/g, "")
-          .normalize("NFD").replace(/[\u0300-\u036f]/g, "")
+          .replace(/[|&;$%@"<>()+,:]/g, "")
           .replace(/ /g, '-')}`;
       },
     },

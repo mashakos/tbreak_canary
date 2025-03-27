7e91451552f559701e7f90567829d010cd06762f
Mohamed Al Arabi <mo.arabi@gmail.com>
2025-03-27 22:48:45 +0200

    updated ytparser.js to match youtube shorts

--- app/utils/ytparser.js
+++ app/utils/ytparser.js
@@ -1,5 +1,6 @@
 export function ytParser(url){
-  var regExp = /^.*((youtu.be\/)|(v\/)|(\/u\/\w\/)|(embed\/)|(watch\?))\??v?=?([^#&?]*).*/;
-  var match = url.match(regExp);
-  return (match&&match[7].length==11)? match[7] : false;
+  var regExp = /(youtu.*be.*)\/(watch\?v=|embed\/|v|shorts|)(.*?((?=[&#?])|$))/gm;
+  var match = regExp.exec(url);
+  console.log(match[3] + " " + regExp);
+  return (match&&match[3].length==11)? match[3] : false;
 }

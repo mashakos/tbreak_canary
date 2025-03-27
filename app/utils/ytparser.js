0edda2cae2a01cef1f3ac374229054eb060aecd6
Mohamed Al Arabi <mo.arabi@gmail.com>
2025-03-27 23:42:01 +0200

    removed console log from ytparser.js

--- app/utils/ytparser.js
+++ app/utils/ytparser.js
@@ -1,6 +1,5 @@
 export function ytParser(url){
   var regExp = /(youtu.*be.*)\/(watch\?v=|embed\/|v|shorts|)(.*?((?=[&#?])|$))/gm;
   var match = regExp.exec(url);
-  console.log(match[3] + " " + regExp);
   return (match&&match[3].length==11)? match[3] : false;
 }

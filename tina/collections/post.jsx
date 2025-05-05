737eec76984a58f5024a3d92af20014a1bb7ab04
Mohamed Al Arabi <mo.arabi@gmail.com>
2025-05-05 12:15:14 +0300

    fixed error with posts collection, error was: Unable to find collection for file at app/routes/undefined.mdx fix was to exclude folder paths

--- tina/collections/post.jsx
+++ tina/collections/post.jsx
@@ -11,7 +11,7 @@ export default {
   label: "Posts",
   path: "app/routes",
   match: {
-    include: 'articles.*',
+    exclude: '{authors/**/**,categories/**/**,homepage/**/**,pages/**/**,tags/**/**}',
   },
   format: 'mdx',
   ui: {

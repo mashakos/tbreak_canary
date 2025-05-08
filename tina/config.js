cace085f99485c23d1a5032dcff31301923f7021
Mohamed Al Arabi <mo.arabi@gmail.com>
2025-05-08 13:50:55 +0300

    removed featured collection from tinacms

--- tina/config.js
+++ tina/config.js
@@ -36,7 +36,7 @@ export default defineConfig({
   },
   // See docs on content modeling for more info on how to setup new content models: https://tina.io/docs/schema/
   schema: {
-    collections: [Home, Page, Category, Author, Tag, Post, Featured],
+    collections: [Home, Page, Category, Author, Tag, Post],
   },
   search: {
     tina: {
